ef91f3f870e74d95f5ff73788b9998c5f4e9cc3c
= <86680816+ehlui@users.noreply.github.com>
2022-06-21 19:01:35 +0200

    Adding ResponseEntity object to specialize response for each endpoint Status codes and body responses using REST good practises

--- src/main/java/org/learning/sprinbootapitrest/persons/PersonController.java
+++ src/main/java/org/learning/sprinbootapitrest/persons/PersonController.java
@@ -3,6 +3,8 @@ package org.learning.sprinbootapitrest.persons;
 import lombok.RequiredArgsConstructor;
 import org.learning.sprinbootapitrest.persons.dto.PersonDTO;
 import org.learning.sprinbootapitrest.persons.dto.PersonName;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,37 +15,52 @@ public class PersonController {
     private final PersonRepository personRepository;
 
     @GetMapping("persons")
-    public List<Person> getPersons() {
-        return personRepository.getAll();
+    public ResponseEntity<?> getPersons() {
+        List<Person> personsList = personRepository.getAll();
+        return personsList.isEmpty() ? ResponseEntity.noContent().build() : ResponseEntity.ok(personsList);
     }
 
     @GetMapping("persons/name/{name}")
-    public List<PersonDTO> getPersonsByMatchingName(@PathVariable String name) {
-        return personRepository.findByName(name);
+    public ResponseEntity<?> getPersonsByMatchingName(@PathVariable String name) {
+        List<PersonDTO> personsDTOList = personRepository.findByName(name);
+        return personsDTOList.size() != 0 ? ResponseEntity.ok(personsDTOList) : ResponseEntity.noContent().build();
     }
 
     @GetMapping("persons/{id}")
-    public PersonDTO getPerson(@PathVariable Integer id) {
-        return personRepository.findById(id);
+    public ResponseEntity<?> getPerson(@PathVariable Integer id) {
+        PersonDTO personDTO = personRepository.findById(id);
+        return personDTO != null ? ResponseEntity.ok(personDTO) : ResponseEntity.noContent().build();
     }
 
     @PostMapping("persons")
-    public PersonDTO createPerson(@RequestBody PersonDTO person) {
-        return personRepository.save(person);
+    public ResponseEntity<?> createPerson(@RequestBody PersonDTO person) {
+        PersonDTO personDTO = personRepository.save(person);
+        return personDTO != null ?
+                ResponseEntity.status(HttpStatus.CREATED).body(person) :
+                ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
     }
 
     @DeleteMapping("persons/{id}")
-    public Person deletePerson(@PathVariable int id) {
-        return this.personRepository.deleteById(id);
+    public ResponseEntity<?> deletePerson(@PathVariable int id) {
+        Person person = personRepository.deleteById(id);
+        return person != null ?
+                ResponseEntity.status(HttpStatus.NO_CONTENT).build() :
+                ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
     }
 
     @PutMapping("persons/{id}")
-    public PersonDTO updatePerson(@PathVariable int id, @RequestBody PersonDTO person) {
-        return this.personRepository.save(person, id); // TODO: Good practices REST -> PUT does not return anything (204 or 404 for resource not found)
+    public ResponseEntity<?> updatePerson(@PathVariable int id, @RequestBody PersonDTO person) {
+        PersonDTO personDTO = personRepository.save(person, id);
+        return personDTO != null ?
+                ResponseEntity.status(HttpStatus.NO_CONTENT).build() :
+                ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
     }
 
     @PatchMapping("persons/{id}")
-    public PersonName patchPerson(@PathVariable int id, @RequestBody PersonName personName) {
-        return this.personRepository.save(personName, id);
+    public ResponseEntity<?> patchPerson(@PathVariable int id, @RequestBody PersonName personName) {
+        PersonName personNameUpdated = personRepository.save(personName, id);
+        return personNameUpdated != null ?
+                ResponseEntity.status(HttpStatus.NO_CONTENT).build() :
+                ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
     }
 }
