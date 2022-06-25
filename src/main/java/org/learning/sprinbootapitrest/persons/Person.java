1a17cd493d8b24b862ccd0f0aa2224a0e7e69aca
= <86680816+ehlui@users.noreply.github.com>
2022-06-25 19:09:00 +0200

    Adding springboot validation api for body request format validation Implementing DTO and main MODEL constraints for request good structure

--- src/main/java/org/learning/sprinbootapitrest/persons/Person.java
+++ src/main/java/org/learning/sprinbootapitrest/persons/Person.java
@@ -4,11 +4,17 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Person {
     private Integer id;
+    @NotBlank(message = "Name is mandatory")
     private String name;
+    @NotBlank(message = "Age is mandatory")
+    @Min(value = 0, message = "Negative age is wrong.")
     private Integer age;
 }
