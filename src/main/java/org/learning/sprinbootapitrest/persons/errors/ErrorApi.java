dabd4f2aa0ca9daf51692044217e8f0480e358f7
= <86680816+ehlui@users.noreply.github.com>
2022-06-25 19:10:52 +0200

    Adding custom exception for person not found and when mapping the json Adding exception handlers for requests endpoints

---
+++ src/main/java/org/learning/sprinbootapitrest/persons/errors/ErrorApi.java
@@ -0,0 +1,24 @@
+package org.learning.sprinbootapitrest.persons.errors;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.*;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorApi {
+    @NonNull
+    private HttpStatus status;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss")
+    private LocalDateTime dateTime = LocalDateTime.now();
+    @NonNull
+    private String message;
+
+    public ErrorApi(HttpStatus status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+}
